bbc6ca9143559a86fb2b113211f59ac1b6cf63da
Jan Carlo De León <janleoncif11@gmail.com>
2024-11-08 12:17:50 -0600

    Inicialización del proyecto

---
+++ index.mjs
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import dotenv from 'dotenv'; 
+
+//Obtener las variables de sesion desde el .env
+dotenv.config();
+
+//Constantes
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY; 
+const OPENAI_API_URL = process.env.OPENAI_API_URL;
+
+export const handler = async (event) => {
+
+
+
+}
